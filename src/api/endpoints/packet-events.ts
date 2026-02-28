b79d013d72cd36d1319cb330403767e950904820
madot13 <madi12122003@gmail.com>
2026-02-28 17:32:49 +0500

    feat: api packet events

--- src/api/endpoints/packet-events.ts
+++ src/api/endpoints/packet-events.ts
@@ -3,12 +3,12 @@ import { IEventRecord, CreateEventPayload } from "../../types/packetevents";
 
 export const packetEventsApi = {
     getAll: async (params?: any) => {
-    const response = await api.get<IEventRecord[]>('/packet-events', { params });
+    const response = await api.get<IEventRecord[]>('/event-payment-types', { params });
     return response.data;},
-    create: (data: CreateEventPayload) => api.post('/packet-events', data),
-    delete: (id: string) => api.delete(`/packet-events/${id}`),
+    create: (data: CreateEventPayload) => api.post('/event-payment-types', data),
+    delete: (id: string) => api.delete(`/event-payment-types/${id}`),
     update: async (id: string, data: Partial<IEventRecord>) => {
-        const response = await api.put(`/packet-events/${id}`, data);
+        const response = await api.put(`/event-payment-types/${id}`, data);
         return response.data;
     },
 };
