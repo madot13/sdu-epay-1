ed95f9d097182cb4031002d71a8121363d52ff0a
madot13 <madi12122003@gmail.com>
2026-03-03 17:33:15 +0500

    fix: remove variant='submit' to apply custom button styles
    - CustomButton with variant='submit' overrides custom className
    - Remove variant to use default variant which respects custom styles
    - Now search button will have proper blue background styling

--- src/components/packet-events/PacketEventsFilter.tsx
+++ src/components/packet-events/PacketEventsFilter.tsx
@@ -96,7 +96,6 @@ export const PacketEventsFilter: FC<PacketEventsFilterProps> = ({ onSearch }) =>
                     />
                 </div>
                 <CustomButton
-                    variant="submit"
                     onClick={handleSearch}
                     className="h-[37px] px-6 mt-auto bg-[#6B9AB0] hover:bg-[#5a8296] text-white transition-colors border-none"
                 >
