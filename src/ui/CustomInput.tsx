d5e22c65179ffde239c45338803482e24778af2a
madot13 <madi12122003@gmail.com>
2026-03-09 20:07:34 +0500

    custom input fix

--- src/ui/CustomInput.tsx
+++ src/ui/CustomInput.tsx
@@ -30,7 +30,7 @@ export const CustomInput: FC<InputProps> = ({
                     <div className="w-[20px] h-[20px] flex absolute top-[50%] left-[20px] transform -translate-y-1/2">
                         {icon}
                         <div className={"relative"}>
-                            <div className={`absolute flex top-[-4px] left-[5px] w-[1px] h-[30px] bg-[#6B9AB0] mx-2 ${error ? 'bg-red-500' : ''} `}></div>
+                            <div className={`absolute flex top-[-4px] left-[5px] w-[1px] h-[30px] bg-[#6B9AB0] mx-2 ${error ? 'bg-red-500' : ''}`}></div>
                         </div>
                     </div>
                 </div>
@@ -42,7 +42,7 @@ export const CustomInput: FC<InputProps> = ({
                 placeholder={placeholder}
                 disabled={disabled}
                 required={required}
-                className={`bg-[#F3F3F3] border border-[#6B9AB0] rounded-[5px] ring-[#6B9AB0] p-[13px] pl-[65px] w-full ${className}  ${error ? 'border-red-500' : ''}`}
+                className={`bg-[#F3F3F3] border border-[#6B9AB0] rounded-[5px] ring-[#6B9AB0] p-[13px] pl-[65px] w-full ${error ? 'border-red-500' : ''} ${disabled ? 'opacity-60 cursor-not-allowed bg-gray-200 border-gray-300' : ''} ${className}`}
             />
         </div>
     );
